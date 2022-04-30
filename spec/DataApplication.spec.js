b1de90110b80169de4b4f5d808f11feffaaf9810
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2022-04-30 09:18:55 +0300

    prepare next version

--- spec/DataApplication.spec.js
+++ spec/DataApplication.spec.js
@@ -1,10 +1,10 @@
-import {DataApplication} from '@themost/data';
+import {TestApplication} from './TestApplication';
 import path from 'path';
 
 describe('DataApplication', () => {
 
     it('should create instance', async () => {
-        const app = new DataApplication(path.resolve(__dirname));
+        const app = new TestApplication(path.resolve(__dirname));
         expect(app).toBeTruthy();
         const context = app.createContext();
         const model = context.model('Category');
