da428761719719bc920d41088907583f54c5b396
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2022-05-03 04:19:05 +0000

    validate promise sequence errors

--- spec/Promise.sequence.spec.js
+++ spec/Promise.sequence.spec.js
@@ -13,12 +13,20 @@ function func2() {
         },500);
     });
 }
+
+function func3() {
+    return new Promise(function(resolve, reject) {
+         setTimeout(function() {
+            return reject('The operation cancelled by the user');
+        },1000);
+    });
+}
 /**
  * 
  * @param {*} promises
  * @returns Promise<Array<*>>
  */
-function PromiseSequence(sources) {
+function promiseSequence(sources) {
     return sources.reduce((promise, func) => (
         promise.then((result) => (
              func().then(Array.prototype.concat.bind(result))
@@ -28,12 +36,18 @@ function PromiseSequence(sources) {
 
 describe('Promise.sequence', () => {
     it('should execute promise sequence', async() => {
-        const results = await PromiseSequence([
+        const results = await promiseSequence([
             () => func1(),
             () => func2()
         ]);
         expect(results).toBeInstanceOf(Array);
         expect(results[0]).toEqual(1);
         expect(results[1]).toEqual(2);
+
+        await expectAsync(promiseSequence([
+            () => func1(),
+            () => func3()
+        ])).toBeRejected();
+
     });
 });
